8c08c554c36f6e2164d88c444ec8a02e8e155aeb
DGTJS <joaodiegom15@gmail.com>
2025-06-15 15:03:36 -0300

    feat: add header and component and banner

---
+++ src/_components/header.tsx
@@ -0,0 +1,23 @@
+import { MenuIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { CardContent, Card } from "./ui/card";
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <Card className="text-gray-800">
+      <CardContent className="flex items-center justify-between">
+        <Image src="/logo.png" alt="Logo" width={120} height={120} />
+        <Button
+          size="icon"
+          variant="outline"
+          className="border-gray-700 hover:bg-gray-700"
+        >
+          <MenuIcon className="h-10 w-10 text-white" />
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,37 @@
 // import Image from "next/image";
+import Header from "@/_components/header";
+import { Input } from "@/_components/ui/input";
+import { SearchIcon } from "lucide-react";
+import { Button } from "@/_components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <div className="p-5 text-white">
+        <h2 className="text-xl">
+          Olá, <span className="text-xl font-bold text-white">Diego!</span>
+        </h2>
+        <p>Domingo, 14 de Junho.</p>
+        <div className="mt-5 flex items-center gap-1 space-x-1 text-gray-800">
+          <Input
+            placeholder="O que você quer fazer hoje?"
+            className="bg-gray-800 placeholder-gray-400"
+          />
+          <Button variant="outline" className="bg-purple-500">
+            <SearchIcon className="h-5 w-5 text-white" />
+          </Button>
+        </div>
+        <div className="relative mt-5 h-[170px] w-full">
+          <Image
+            src="/banner-01.png"
+            alt="Banner"
+            fill
+            className="rounded-lg object-cover"
+          />
+        </div>
+      </div>
+    </>
+  );
 }

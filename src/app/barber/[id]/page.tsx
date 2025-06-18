46cdf62bdea00d2c39b094f37ad28a24334f92dc
DGTJS <joaodiegom15@gmail.com>
2025-06-18 17:41:44 -0300

    feat(barber): add  new elements to the BarberPage

--- src/app/barber/[id]/page.tsx
+++ src/app/barber/[id]/page.tsx
@@ -1,5 +1,8 @@
 import { db } from "@/_lib/prisma";
 import Image from "next/image";
+import { Button } from "@/_components/ui/button";
+import { ChevronLeftIcon, MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 interface BarbersProps {
   params: {
@@ -11,8 +14,31 @@ const BarberPage = async ({ params }: BarbersProps) => {
   const Barbers = await db.barber.findUnique({ where: { id: params.id } });
   return (
     <>
-      <h1>{Barbers!.name}</h1>
-      <Image src={Barbers!.photo!} alt="Logo" width={120} height={120} />
+    <div>
+      <div className="relative h-[300] w-full">
+      <Image src={Barbers!.photo!} alt="Logo" fill className="object-cover object-top"/>
+
+      </div>
+
+      <Button  variant="outline" className="absolute top-4 left-4 cursor-pointer">
+        <Link href="/">
+        
+        <ChevronLeftIcon className="text-white" />
+        </Link>
+
+      </Button>
+
+      <Button variant="outline" className="absolute top-4 right-4">
+        <MenuIcon/>
+      </Button>
+
+      <div className="p-2">
+
+      <h1 className="text-2xl font-semibold">{Barbers!.name}</h1>
+      <p className="text-gray-500">{Barbers!.phone}</p>
+      </div>
+    </div>
+
     </>
   );
 };
